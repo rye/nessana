77cb8d9c1bf3ec5d73d95393dc2d2dc5d528ad9f
Kristofer Rye <kristofer.rye@gmail.com>
2018-06-26 14:30:14 -0500

    Migrate ExecutionConfiguration into its own file

--- lib/nessana/executor.rb
+++ lib/nessana/executor.rb
@@ -1,77 +1,31 @@
-require 'nessana/version'
+require 'nessana/executor/execution_configuration'
 require 'optparse'
 
 module Nessana
 	module Executor
-		class ExecutionConfiguration
-			attr_accessor :from_dump, :to_dump
-			attr_accessor :verbosity
-
-			def initialize
-				verbosity = :info
-			end
-
-			def add_parser_hooks(parser)
-				parser.banner = "Usage: #{ARGV[0]} [options]"
-				parser.separator ""
-				parser.separator "Execution Options"
-
-				add_from_dump_option(parser)
-				add_to_dump_option(parser)
-				add_verbosity_option(parser)
-
-				parser.separator ""
-				parser.separator "General Options"
-				add_usage_option(parser)
-				add_verbosity_option(parser)
-
-				parser.on_tail("-h", "--help", "Show this message") do
-					puts parser
-					exit
-				end
-
-				parser.on_tail("--version", "Show version") do
-					puts VERSION
-					exit
-				end
-			end
-
-			protected
-
-			def add_from_dump_option(parser)
-				parser.on('-f', '--from FILENAME', "The filename to load as the original dump.")
-			end
-
-			def add_to_dump_option(parser)
-				parser.on('-t', '--to FILENAME', "The filename to load as the new dump.")
-			end
-
-			def add_usage_option(parser)
-				parser.on('-h', '--help', "Print usage summary.") do
-					puts parser
-					exit 0
-				end
-			end
-
-			def add_verbosity_option(parser)
-				parser.on('-v', '--verbosity VERBOSITY', "The level of verbosity to use.")
-			end
-		end
-
 		def self.print_usage!
 			puts @parser
 		end
 
 		def self.execute!(*argv)
-			configuration = parse!(*argv)
+			parse!(*argv)
+
+			unless @configuration[:dump_filename]
+				puts 'No dump filename given; not doing anything.'
+				exit 0
+			end
+
+			raise 'dump_filename nil!' unless !@configuration[:dump_filename].nil?
+			raise 'cache_filename nil!' unless !@configuration[:cache_filename].nil?
+
+			dump = Dump.from_file(@configuration[:dump_filename])
 		end
 
-		protected
+		def self.parse(*argv)
+			configuration = ExecutionConfiguration.new
 
-		def self.parse!(*argv)
-			@configuration = ExecutionConfiguration.new
-			@parser = OptionParser.new do |parser|
-				@configuration.add_parser_hooks(parser)
+			option_parser = OptionParser.new do |parser|
+				configuration.add_parser_hooks(parser)
 
 				if argv.count == 0
 					puts parser
@@ -80,7 +34,17 @@ module Nessana
 
 				parser.parse(*argv)
 			end
-			@configuration
+
+			configuration[:dump_filename] = argv.shift
+
+			configuration
 		end
+
+		protected
+
+		def self.parse!(*argv)
+			@configuration = parse(*argv)
+		end
+
 	end
 end
---
+++ lib/nessana/executor/execution_configuration.rb
@@ -0,0 +1,87 @@
+require 'nessana/version'
+
+module Nessana::Executor
+	class ExecutionConfiguration < ::Hash
+		def initialize
+			self[:verbosity] = :info
+			self[:config] = 'config.yml'
+			self[:dump_filename] = nil
+		end
+
+		def add_parser_hooks(parser)
+			parser.banner = "Usage: #{ARGV[0]} [options] <filename.csv>"
+			parser.separator ""
+			parser.separator "Execution Options"
+
+			add_config_option(parser)
+
+			parser.separator ""
+			parser.separator "General Options"
+			add_usage_option(parser)
+			add_verbosity_option(parser)
+
+			parser.on_tail("-h", "--help", "Show this message") do
+				puts parser
+				exit
+			end
+
+			parser.on_tail("--version", "Show version") do
+				puts Nessana::VERSION
+				exit
+			end
+		end
+
+		def read_configuration(filename)
+			raise ArgumentError, 'Must pass a valid filename' unless !filename.nil?
+
+			mime_type = infer_mime_type(filename)
+			parsed = nil
+
+			begin
+				io = IO.open(filename, 'rb')
+				data = io.read
+
+				case mime_type
+				when /yaml/
+					require 'yaml'
+					parsed = YAML.load(data)
+				when /json/
+					require 'json'
+					parsed = JSON.parse(data)
+				end
+			ensure
+				io.close unless io.closed?
+			end
+
+			parsed.to_h
+		end
+
+		protected
+
+		def add_config_option(parser)
+			parser.on('-c', '--config CONFIG', "Load configuration from CONFIG (default: #{self[:config]})") do |config|
+				self[:config] = config
+				read_configuration!
+			end
+		end
+
+		def add_usage_option(parser)
+			parser.on('-h', '--help', "Print usage summary.") do
+				puts parser
+				exit 0
+			end
+		end
+
+		def add_verbosity_option(parser)
+			parser.on('-v', '--verbosity VERBOSITY', "The level of verbosity to use. (default: #{self[:verbosity]})")
+		end
+
+		def read_configuration!
+			self.merge(read_configuration(self[:config]))
+		end
+
+		def infer_mime_type(filename)
+			MIME::Types.type_for(filename).first.content_type
+		end
+	end
+end
