8f89ac100bac53d0ce912486297137ce140a0fab
Kristofer Rye <kristofer.rye@gmail.com>
2018-07-08 08:52:57 -0500

    Add a Nessana::Cache spec
    (Is it a class?)

--- spec/lib/nessana/cache_spec.rb
+++ spec/lib/nessana/cache_spec.rb
@@ -3,3 +3,11 @@ describe 'nessana/cache' do
 end
 
 require 'nessana/cache'
+
+describe Nessana do
+	describe '::Cache' do
+		subject { Nessana::Cache }
+
+		it { is_expected.to be_a(Class)}
+	end
+end
