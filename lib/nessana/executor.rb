cae5b433e05f7e1ac5ad403601f3df02b1c27d1c
Kristofer Rye <kristofer.rye@gmail.com>
2018-07-11 07:55:56 -0500

    Clean up executor and vulnerability classes (profile, too!)

--- lib/nessana/executor.rb
+++ lib/nessana/executor.rb
@@ -1,5 +1,7 @@
-require 'pp'
 require 'optparse'
+require 'pp'
+require 'ruby-prof'
+require 'ruby-prof-flamegraph'
 
 require 'nessana/executor/execution_configuration'
 require 'nessana/filter'
@@ -13,6 +15,8 @@ module Nessana
 
 		def self.execute!(argv = ARGV)
 			parse!(*argv)
+
+			RubyProf.start if !!@configuration['performance']
 
 			unless @configuration['old_filename']
 				$stderr.puts 'No old dump filename given; will assume no prior knowledge.'
@@ -32,51 +36,92 @@ module Nessana
 			old_dump = @configuration['old_filename'] ? Dump.new(@configuration['old_filename'], filters) : Dump.new
 			new_dump = Dump.new(@configuration['new_filename'], filters)
 
+			# diff = new_dump.old_old_dump
+
+			# puts "The following vulnerabilities are NEW:"
+
+			# diff[:added_vulnerabilities].each do |new_v|
+			# 	puts new_v
+			# 	puts "=" * 80
+			# end
+
+			# puts "\n" * 4
+
+			# puts "The following detections are NEW:"
+
+			# diff[:added_detections].each do |new_d|
+			# 	vulnerability = new_d[:vulnerability]
+			# 	detections = new_d[:detections].to_a
+
+			# 	puts vulnerability.short_description
+			# 	resulting_string = detections.map do |detection|
+			# 		"+ #{detection.to_s}"
+			# 	end.join("\n")
+			# 	puts resulting_string
+			# end
+
+			# puts "\n" * 4
+
+			# puts "The following detections were FIXED:"
+
+			# diff[:removed_detections].each do |fixed|
+			# 	vulnerability = fixed[:vulnerability]
+			# 	detections = fixed[:detections].to_a
+
+			# 	puts vulnerability.short_description
+			# 	resulting_string = detections.map do |detection|
+			# 		"- #{detection.to_s}"
+			# 	end.join("\n")
+			# 	puts resulting_string
+			# end
+
+			# puts "\n" * 4
+
+			# puts "The following vulnerabilities were FIXED completely!"
+
+			# diff[:removed_vulnerabilities].each do |fixed|
+			# 	puts fixed.title_line
+			# end
+
 			diff = new_dump - old_dump
 
-			puts "The following vulnerabilities are NEW:"
+			puts "The following vulnerabilities have been removed:"
 
-			diff[:new_vulnerabilities].each do |new_v|
-				puts new_v
-				puts "=" * 80
+			diff[:removed_vulnerabilities].each do |v|
+				puts v.short_description
 			end
 
 			puts "\n" * 4
 
-			puts "The following detections are NEW:"
+			puts "The following new vulnerabilities have been detected:"
 
-			diff[:new_detections].each do |new_d|
-				vulnerability = new_d[:vulnerability]
-				detections = new_d[:detections].to_a
+			diff[:removed_vulnerabilities].each do |v|
+				puts v
 
-				puts vulnerability.short_description
-				resulting_string = detections.map do |detection|
-					"+ #{detection.to_s}"
-				end.join("\n")
-				puts resulting_string
+				puts "\n" * 2
 			end
 
-			puts "\n" * 4
+			puts "The following changes have been made to discoveries:"
 
-			puts "The following detections were FIXED:"
-
-			diff[:fixed_detections].each do |fixed|
-				vulnerability = fixed[:vulnerability]
-				detections = fixed[:detections].to_a
-
-				puts vulnerability.short_description
-				resulting_string = detections.map do |detection|
-					"- #{detection.to_s}"
-				end.join("\n")
-				puts resulting_string
+			diff[:all_vulnerabilities].each do |v|
+				puts v.title_line
+				v.detections.sort do |detection_a, detection_b|
+					detection_a[:port] <=> detection_b[:port]
+				end.sort do |detection_a, detection_b|
+					detection_a[:host] <=> detection_b[:host]
+				end.each do |detection|
+					if detection[:status] && detection[:status] != true
+						puts detection.to_s
+					end
+				end
+				puts "\n" * 2
 			end
 
-			puts "\n" * 4
-
-			puts "The following vulnerabilities were FIXED completely!"
-
-			diff[:mitigated_vulnerabilities].each do |fixed|
-				puts fixed.title_line
+			if !!@configuration['performance']
+				result = RubyProf.stop
+				printer = RubyProf::FlameGraphPrinter.new(result)
+				io = open(@configuration['performance'], 'wb')
+				printer.print(io, {})
 			end
 
 			# Detections: print top line and synopsis for
