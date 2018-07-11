e7fde69ee1860287b42d58eaf40b35c50d6fb547
Kristofer Rye <kristofer.rye@gmail.com>
2018-07-11 07:37:52 -0500

    Make status just another field in Detection

--- lib/nessana/detection.rb
+++ lib/nessana/detection.rb
@@ -1,24 +1,24 @@
 module Nessana
 	class Detection < Hash
-		attr_reader :status
-
 		def initialize(host, protocol, port, status = nil)
 			self[:host], self[:protocol], self[:port] = host, protocol, port
-			@status = status
+			self[:status] = status
 		end
 
 		def to_s
-			"#{@status ? status_prefix : ''}#{self[:host]}:#{self[:port]}/#{self[:protocol]}"
+			"#{self[:status] ? status_prefix : ''}#{self[:host]}:#{self[:port]}/#{self[:protocol]}"
 		end
 
 		protected
 
 		def status_prefix
-			case @status
+			case self[:status]
 			when :added
 				'+ '
 			when :removed
 				'- '
+			when :present
+				'  '
 			else
 				'? '
 			end
