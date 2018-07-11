ee5810e0641b1a08983d5eff1563ba8f3bc26c66
Kristofer Rye <kristofer.rye@gmail.com>
2018-07-10 23:54:30 -0500

    Add a to_s helper for Detections

--- lib/nessana/detection.rb
+++ lib/nessana/detection.rb
@@ -3,5 +3,9 @@ module Nessana
 		def initialize(host, protocol, port)
 			self[:host], self[:protocol], self[:port] = host, protocol, port
 		end
+
+		def to_s
+			"#{self[:host]}:#{self[:port]}/#{self[:protocol]}"
+		end
 	end
 end
