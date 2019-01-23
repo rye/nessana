167fe3e95097a7f64c58da1a975118fb6985fcae
Kristofer Rye <kristofer.rye@gmail.com>
2019-01-22 18:06:25 -0600

    dump_spec: Add a dummy spec for Dump.read
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- spec/lib/nessana/dump_spec.rb
+++ spec/lib/nessana/dump_spec.rb
@@ -8,4 +8,13 @@ describe Nessana::Dump do
 	it 'inherits from Hash' do
 		expect(subject.class.ancestors).to include(Hash)
 	end
+
+	describe '.read' do
+		it 'reads from a file' do
+			allow(File).to receive(:open)
+			expect(File).to receive(:open).with('test file', 'rb')
+
+			Nessana::Dump.read('test file')
+		end
+	end
 end
